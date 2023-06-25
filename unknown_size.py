7711480bf1d3ba0669597c65be31bf8442078722
Dennis <zdq198654@gmail.com>
2023-06-25 14:34:28 -0400

    unKnown size

---
+++ unknown_size.py
@@ -0,0 +1,4 @@
+# Given a sorted dictionary with unknown size, how to determine whether a word is in this dictionary or not
+# Example: dictionary[x] = {1,3,5,7,9,......1000  ......1000000000.......}
+# target == 9999
+# Assumption if a[index] == null, then we know the size of dictionary is < index
